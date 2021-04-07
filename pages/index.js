69a11ec0d6bbf658eba55d38cffba84f1be1f9ec
AmoDinho <u14284783@tuks.co.za>
2021-04-07 23:04:05 +0200

    added airtable func

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,38 @@
 import App from "../components/App";
-
+import { base } from "../lib/airtable";
 const IndexPage = () => (
   <App>
     <h1 className="text-green-500">WELCOME TO SASSY WORLD</h1>
   </App>
 );
+
+export async function getStaticProps() {
+  base("Products")
+    .select({
+      // Selecting the first 3 records in Grid view:
+      maxRecords: 3,
+      view: "Grid view",
+    })
+    .eachPage(
+      function page(records, fetchNextPage) {
+        // This function (`page`) will get called for each page of records.
+
+        records.forEach(function (record) {
+          console.log("Retrieved", record.get("ID"));
+        });
+
+        // To fetch the next page of records, call `fetchNextPage`.
+        // If there are more records, `page` will get called again.
+        // If there are no more records, `done` will get called.
+        fetchNextPage();
+      },
+      function done(err) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+      }
+    );
+}
 
 export default IndexPage;
